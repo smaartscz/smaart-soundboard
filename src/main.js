cb696265f4c3073f4768bb7b93e50e108da19908
smaartscz <65630155+smaartscz@users.noreply.github.com>
2023-01-29 02:50:22 +0100

    Fixed small issues
    Now is stop button working and only one audio will be played at the same time!

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+var audio = new Audio();
+function isPlaying(){
+  if(audio.duration > 0 && !audio.paused)
+  return;
+}
+
 function play(clicked_id) {
-    var audio = new Audio("audio/" + clicked_id + ".mp3");
-    audio.play();
-  }
-  function stop() {
-    const audio = document.querySelector("#audio");
-    pause();
-    audio.currentTime = 0;
-  }
+  isPlaying();
+  audio.src = "audio/" + clicked_id + ".mp3"
+  audio.play();
+}
+
+function stop() {
+  audio.pause();
+  audio.currentTime = 0;
+}
