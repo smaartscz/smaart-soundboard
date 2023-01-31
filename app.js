67563e2ba1b7a26a45643d882079ee65c4af3fa1
smaartscz <65630155+smaartscz@users.noreply.github.com>
2023-01-31 02:35:59 +0100

    Huge rework
    Reworked loading json files, now it uses completely new function called getJSON(same name as old function but now it uses fs instead of GET request.
    
    Added new function saveJSON to save json files in custom location(inside cfg folder ofc)

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1892,
     height: 301,
     autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true, // <--- flag
+      nodeIntegrationInWorker: true, // <---  for web workers
+      contextIsolation: false
+   }
   })
 
   win.loadFile('./src/index.html')
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ var audio = new Audio();
 const output = document.querySelector('.output');
 var JSONfile =[];
 
-document.addEventListener('DOMContentLoaded', function() {
-  getJSON();
-}, false);
+document.addEventListener('DOMContentLoaded', loadButtons(), false);
+
+function loadButtons(){
+  getJSON("buttons.json");
+  setTimeout(() => {  createHTML(JSONfile); }, 50);
+}
 
 function isPlaying(){
   if(audio.duration > 0 && !audio.paused)
@@ -21,18 +24,29 @@ function stop() {
   audio.pause();
   audio.currentTime = 0;
 }
-function getJSON(){
 
-  var request = new XMLHttpRequest();
-  request.open("GET", "./cfg/settings.json", false);
-  request.send(null)
-  JSONfile = JSON.parse(request.responseText);
-  createHTML(JSONfile);
-  return JSONfile;
+function saveJSON(data, file){
+  file = "./src/cfg/" + file;
+  const fs = require("fs");
+  fs.writeFileSync(file, JSON.stringify(data));
+  console.log(JSON.stringify(data));
+  console.log("JSONfile was saved successfully!");
 }
 
+function getJSON(file){
+  const fs = require("fs");
+  file = "./src/cfg/" + file;
+  fs.readFile(file, "utf8", (err, JSONString) => {
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }
+    JSONfile = JSON.parse(JSONString);
+  });
+  return;
+}
 
-function createHTML(value){
+async function createHTML(value){
   let html = '<button id="stop" style="text-align: center; width:100%; height:20%" class="btn btn-danger" onClick="stop()">STOP PANIK EMERGENCY</button> ';
   console.log(value);
   value.forEach(function(nazev){
