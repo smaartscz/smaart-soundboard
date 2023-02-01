e2963931960cbe45f0a3e4d985d762102842953e
smaartscz <65630155+smaartscz@users.noreply.github.com>
2023-02-01 18:52:50 +0100

    Update 0.2.3!
    + Working audio output selector!

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const audio = new Audio();
 const output = document.querySelector('.output');
-
+var test 
 //Load buttons on startup
 document.addEventListener('DOMContentLoaded', loadButtons(), false); 
 
 async function loadButtons(){
   const file = await getJSON("buttons.json");
-  htmlButtons(file); //hotfix
+  htmlButtons(file);
 }
 async function createSettings(){
   const file = await getJSON("settings.json");
@@ -32,10 +32,9 @@ function stop() {
 }
 //Writing data into JSON file
 function saveJSON(data, file){
-  file = "./src/cfg/" + file;
+  file = "./cfg/" + file;
   const fs = require("fs");
   fs.writeFileSync(file, JSON.stringify(data));
-  console.log(JSON.stringify(data));
   console.log("JSONfile was saved successfully!");
 }
 
@@ -43,7 +42,7 @@ function saveJSON(data, file){
 async function getJSON(file) {
   const fs = require("fs");
   return new Promise((resolve, reject) => {
-   fs.readFile("./src/cfg/" + file, "utf8", (err, JSONString) => {
+   fs.readFile("./cfg/" + file, "utf8", (err, JSONString) => {
       if (err) {
       console.log("File read failed:", err);
        reject(err);
@@ -58,36 +57,45 @@ function getAudioDevices(){
 navigator.mediaDevices.enumerateDevices()
   .then(devices => {
     const outputDevices = devices.filter(device => device.kind === 'audiooutput');
-    const option = document.createElement('option');
+    const audioOutputSelect = document.getElementById('audioOutputSelect');
     outputDevices.forEach(device => {
+      const option = document.createElement('option');
       option.value = device.deviceId;
       option.text = device.label || `Device ${device.deviceId}`;
+      audioOutputSelect.appendChild(option);
     });
 });
 }
 
 //Set audio output to selected audio device
 async function setAudioDevice(){
-  //add function that will read audio settings from /cfg/settings.json and save them
+  //add function that will read audio settings from /cfg/settings.json
   let output;
   const settings = await getJSON("settings.json");
     settings.forEach(async (ele) => {
       if (ele.audio){
         output = ele.audio.outputId;
       }
-      console.log(ele);
-      console.log(output);
       await audio.setSinkId(output);
-
     })
-  //audioContext.destination = audioContext.outputs.get(selectedDeviceId);
 }
+async function saveSettings(){
+const AudioDevice = document.getElementById("audioOutputSelect");
+const AudioDeviceId = AudioDevice.value;
+const AudioDeviceName = AudioDevice.options[AudioDevice.selectedIndex].text;
+console.log("Received request for changing audio settings!");
+var data = await getJSON("settings.json");
+
+data[0].audio.output = AudioDeviceName
+data[0].audio.outputId = AudioDeviceId
+saveJSON(data, "settings.json");
+loadButtons();
+}
+
 
 //Generating HTML for settings page
 async function htmlSettings(value) {
   let html = '<h1>SETTINGS</h1>';
-  console.log(value);
-
   html += '<h2>General Settings</h2>';
   value.forEach(function(general) {
     //generate buttons for general settings(language, etc)
@@ -97,16 +105,20 @@ async function htmlSettings(value) {
   html += '<h2>Audio Settings</h2>';
   value.forEach(function(audio) {
     //generate buttons for audio settings(device output, etc)
-    console.log(audio.audio.output);
-    html += `Current audio output: ${audio.audio.output}`;
+    html += `Current audio output: ${audio.audio.output}<br>`;
+    html += '<select id="audioOutputSelect">';
+    html += '</select>';
+    html += '<br><button id="save" class="btn btn-save" onClick="saveSettings()">Save</button> ';
   });
 
+  getAudioDevices();
+  
+  html += '<button id="cancel" class="btn btn-cancel" onClick="loadButtons()">Cancel</button>'
   output.innerHTML = html;
 }
 //Generating HTML for buttons
 async function htmlButtons(value){
-  let html = '<button id="stop"  class="btn btn-stop" onClick="stop()">STOP PANIK EMERGENCY</button> ';
-  console.log(value);
+  let html = '<span><button id="stop" class="btn btn-stop" onClick="stop()">STOP PANIK EMERGENCY</button><button id="settings" class="btn" onClick="createSettings();">Settings</button></span>';
   value.forEach(function(nazev){
     html += `<h2 style="text-align: center;">${nazev.name}</h2><p>`
     nazev.files.forEach(function(file, index){
