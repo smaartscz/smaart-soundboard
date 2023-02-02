a41b5da278ef3502d328c1eb889a7103f990a6e3
smaartscz <65630155+smaartscz@users.noreply.github.com>
2023-02-02 03:49:51 +0100

    First full release!

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+let fileName;
+
 const audio = new Audio();
 const output = document.querySelector('.output');
 //Load buttons on startup
@@ -31,7 +33,7 @@ function stop() {
 }
 //Writing data into JSON file
 function saveJSON(data, file){
-  file = "./src/cfg/" + file;
+  file = "cfg/" + file;
   const fs = require("fs");
   fs.writeFileSync(file, JSON.stringify(data));
   console.log("JSONfile was saved successfully!");
@@ -41,7 +43,7 @@ function saveJSON(data, file){
 async function getJSON(file) {
   const fs = require("fs");
   return new Promise((resolve, reject) => {
-   fs.readFile("./src/cfg/" + file, "utf8", (err, JSONString) => {
+   fs.readFile("cfg/" + file, "utf8", (err, JSONString) => {
       if (err) {
       console.log("File read failed:", err);
        reject(err);
@@ -79,7 +81,7 @@ async function setAudioDevice(){
     })
 }
 //Save audio settings picked up from select 
-async function saveSettings(){
+async function saveAudioSettings(){
 const AudioDevice = document.getElementById("audioOutputSelect");
 const AudioDeviceId = AudioDevice.value;
 const AudioDeviceName = AudioDevice.options[AudioDevice.selectedIndex].text;
@@ -91,7 +93,68 @@ saveJSON(data, "settings.json");
 loadButtons();
 }
 
+async function removeCategory(value){
+  var file = await getJSON("buttons.json");
+  console.log("Received request for category removal: " + value)
+  file.splice(value, 1);
+  saveJSON(file, "buttons.json");
+  buttonSettings()
+}
 
+async function removeSound(index, audioFile){
+  const file = getJSON("buttons.json");
+  console.log("Received request for sound removal: ")
+  console.log(index, file);
+}
+
+async function createNewCategory(name){
+  var file = await getJSON("buttons.json");
+  console.log("Received request for creating new category: ");
+  const newCategoryName = document.getElementById("newCategoryName");
+  console.log(newCategoryName.value);
+  const index = Object.keys(file).length;
+  console.log(index);
+  file[index] = {};
+  file[index].category = newCategoryName.value;
+  file[index].files = [];
+  file[index].fancy = [];
+  saveJSON(file, "buttons.json");
+  buttonSettings();
+}
+
+async function createNewSound(){
+  var file = await getJSON("buttons.json");
+  const newAudioName = document.getElementById("newAudioName").value;
+  const newAudioCategory = document.getElementById("newAudioCategory").value;
+  console.log(newAudioName + " " + newAudioCategory);
+  file[newAudioCategory].files.push(fileName);
+  file[newAudioCategory].fancy.push(newAudioName);
+  console.log(file);
+  saveJSON(file, "buttons.json");
+  buttonSettings();
+}
+//Function to handle file upload
+function handleFileUpload(event){
+  const fs = require('fs');
+  const reader = new FileReader();
+  const files = event.target.files;
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    fileName = file.name.replace(/[^\w\s]/gi, '').replace(/\s/g, '') + ".mp3";
+    const filePath = `audio/${fileName}`;
+    reader.onload = function() {
+      fs.writeFile(filePath, Buffer.from(reader.result), (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`File saved to ${filePath}`);
+        }
+      });
+    };
+    reader.readAsArrayBuffer(file);
+  }
+}
 //Generating HTML for settings page
 async function htmlSettings(value) {
   let html = '<h1>SETTINGS</h1>';
@@ -105,21 +168,59 @@ async function htmlSettings(value) {
   value.forEach(function(audio) {
     //generate buttons for audio settings(device output, etc)
     html += `Current audio output: ${audio.audio.output}<br>`;
-    html += '<select class="audio-select" id="audioOutputSelect">';
-    html += '</select>';
-    html += '<br><button id="save" class="btn btn-save" onClick="saveSettings()">Save</button> ';
+    html += '<select class="audio-select" id="audioOutputSelect"></select>';
   });
 
   getAudioDevices();
-  
-  html += '<button id="cancel" class="btn btn-cancel" onClick="loadButtons()">Cancel</button>'
+  html += '<br><button class="btn btn-primary" onClick="buttonSettings()">Edit buttons</button><button id="save" class="btn btn-save" onClick="saveAudioSettings()">Save</button> <button id="cancel" class="btn btn-cancel" onClick="loadButtons()">Cancel</button>'
+  output.innerHTML = html;
+}
+async function buttonSettings(){
+  const value = await getJSON("buttons.json")
+  let html = '<h1>Buttons settings</h1>';
+  html += '<h2>Categories <button id="createNewCategory" class="btn btn-newCategory" onClick="htmlNewCategory()">Create New Category</button></h2>';
+  value.forEach(function(nazev, index){
+    html += `${nazev.category} - <span><button id="${index}" class="btn btn-delete" onClick="removeCategory(this.id)">Remove</button></span><p>`
+    });
+    html += '<h2>Sounds <button id="addNewSound" class="btn btn-newCategory" onClick="htmlNewSound()">Add new sound</button></h2>';
+    value.forEach(function(nazev){
+      nazev.files.forEach(function(file, index){
+        html += `<span>${nazev.fancy[index]} - <button id="${file}" class="btn btn-delete" onClick="removeSound(this.id)">Remove</button></span><p> `
+      });
+    });
+  html += '<br><button id="cancel" class="btn btn-cancel" onClick="loadButtons()">Go back</button>'
+  output.innerHTML = html;
+}
+//Create new HTML for adding audio sounds
+async function htmlNewSound(value){
+  let html = '<h1>Add new category</h1>';
+  html += 'Enter sound effect name: '
+  html += '<input type="text" id="newAudioName"><p>'
+  html += 'Select category: '
+  html += '<select id="newAudioCategory">';
+  const categories = await getJSON("buttons.json");
+  categories.forEach(function(category, index) {
+    html += `<option value="${index}">${category.category}</option>`;
+  });
+  html += '</select><p>';
+  html += 'Upload your audio file: '
+  html += '<input type="file" onchange="handleFileUpload(event)">'
+  html += '<br><button class="btn btn-save" onClick="createNewSound()">Save</button> <button id="cancel" class="btn btn-cancel" onClick="buttonSettings()">Cancel</button>'
+  output.innerHTML = html;
+}
+async function htmlNewCategory(){
+  let html = '<h1>Add new category</h1>';
+  html += 'Enter category name: '
+  html += '<input type="text" id="newCategoryName">'
+  html += '<br><button class="btn btn-save" onClick="createNewCategory()">Save</button> <button id="cancel" class="btn btn-cancel" onClick="buttonSettings()">Cancel</button>'
   output.innerHTML = html;
 }
 //Generating HTML for buttons
-async function htmlButtons(value){
-  let html = '<span><button id="stop" class="btn btn-stop" onClick="stop()">STOP PANIK EMERGENCY</button><button id="settings" class="btn" onClick="createSettings();">Settings</button></span>';
+async function htmlButtons(){
+  const value = await getJSON("buttons.json")
+  let html = '<span><button id="stop" class="btn btn-stop" onClick="stop()">STOP PANIK EMERGENCY</button><button id="settings" class="btn btn-primary" onClick="createSettings();">Settings</button></span>';
   value.forEach(function(nazev){
-    html += `<h2 style="text-align: center;">${nazev.name}</h2><p>`
+    html += `<h2 style="text-align: center;">${nazev.category}</h2><p>`
     nazev.files.forEach(function(file, index){
       html += `<span><button id="${file}" class="btn btn-primary" onClick="play(this.id)">${nazev.fancy[index]}</button></span> `
     });
