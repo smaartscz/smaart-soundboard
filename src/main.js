18d08997e86f02fc8a293e829186d484e08dce79
smaartscz <65630155+smaartscz@users.noreply.github.com>
2023-02-05 21:20:23 +0100

    Feature update 2!
    User now can set custom color to buttons! ( #17 )
    
    Settings are now translated to czech! ( #20 )
    
    Also if you somehow fck up your settings you can reset them ( #23 )
    
    Fixed issue with removeSound() ( #22 )
    
    If you enable mixing your mic with sound effects now it will work everywhere ( #19 )

--- src/main.js
+++ src/main.js
@@ -1,22 +1,15 @@
-const { changeLanguage } = require("i18next");
-const i18n = require('i18next');
 const audio = new Audio();
 const output = document.querySelector('.output');
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-let fileName;
+let fileName, test;
 //Load buttons on startup
 document.addEventListener('DOMContentLoaded', htmlButtons, false); 
 
-i18n.init({
-  resources: {
-    en: {
-      translation: require('./locales/en.json')
-    },
-    cs: {
-      translation: require('./locales/cs.json')
-    }
-  }
+var Translator = require('@andreasremdt/simple-translator');
+var translator = new Translator({
+  filesLocation: 'locales/',
 });
+
 
 //Checking if any audio is playing
 function isPlaying(){ 
@@ -61,18 +54,18 @@ async function getJSON(file) {
 async function createJSON(value) {
   console.log("Received request for creating: " + value)
   const fs = require("fs");
-  let file = [];
+  let data = [];
   switch(value) {
     case "buttons.json":
      await fs.promises.mkdir("cfg/", { recursive: true });
-      await fs.promises.writeFile("cfg/buttons.json", JSON.stringify(file), "utf8");
+      await fs.promises.writeFile("cfg/buttons.json", JSON.stringify(data), "utf8");
       console.log("File successfully written.");
       htmlButtons();
     break;
     case "settings.json":
-      file.push({"general": {"language": "en"}, "audio": {"outputId": "default", "fileVolume": "100", "output": "Default audio output on your device!", "allowMixing":false, "inputId": "default", "input":"Default audio input on your device!"}});
+      data.push({"general": {"languageCode": "en", "languageName": "English"}, "audio": {"outputId": "default", "fileVolume": "100", "output": "Default audio output on your device!", "allowMixing":false, "inputId": "default", "input":"Default audio input on your device!"}});
       await fs.promises.mkdir("cfg/", { recursive: true });
-      await fs.promises.writeFile("cfg/settings.json", JSON.stringify(file), "utf8");
+      await fs.promises.writeFile("cfg/settings.json", JSON.stringify(data), "utf8");
       console.log("File successfully written.");
       htmlSettings();
     break;
@@ -122,6 +115,7 @@ async function setOutputAudioDevice(){
 
 //Save settings picked up from htmlSettings
 async function saveSettings(){
+
 //Get audio output settings
 const outputAudioDevice = document.getElementById("audioOutputSelect");
 const outputAudioDeviceId = outputAudioDevice.value;
@@ -138,10 +132,13 @@ const inputAudioDeviceId = inputAudioDevice.value;
 //Get state of allowMixing
 const allowMixing = document.getElementById("allowMixing").checked;
 
-//Get state of allowMixing
-const language = document.getElementById("languageSelect").value;
+//Get state of language
+const language = document.getElementById("languageSelect");
+const languageCode = language.value;
+const languageName = language.options[language.selectedIndex].text
 
-console.log("Received request for changing audio settings!");
+
+console.log("Received request for changing settings!");
 var data = await getJSON("settings.json");
 
 //Store audio output settings into object
@@ -159,7 +156,8 @@ data[0].audio.inputId = inputAudioDeviceId
 data[0].audio.allowMixing = allowMixing
 
 //Save selected language
-data[0].general.language = language
+data[0].general.languageCode = languageCode
+data[0].general.languageName = languageName
 
 //Store data into settings.json
 saveJSON(data, "settings.json");
@@ -168,7 +166,7 @@ saveJSON(data, "settings.json");
 htmlButtons();
 
 //Reload webpage to refresh variable
-window.location.reload();
+//window.location.reload();
 }
 
 //Remove category selected by user
@@ -190,6 +188,7 @@ async function removeSound(audioFile){
     if (index !== -1) {
       data[i].files.splice(index, 1);
       data[i].fancy.splice(index, 1);
+      data[i].color.splice(index, 1);
     }
 }
 fs.unlink('audio/' + audioFile, (err) => {
@@ -205,13 +204,14 @@ async function createNewCategory(name){
   var data = await getJSON("buttons.json");
   console.log("Received request for creating new category: " + name);
   const newCategoryName = document.getElementById("newCategoryName");
-  const index = Object.keys(file).length;
+  const index = Object.keys(data).length;
 
   //Template for category
   data[index] = {};
   data[index].category = newCategoryName.value;
   data[index].files = [];
   data[index].fancy = [];
+  data[index].color = [];
 
   //Save data
   saveJSON(data, "buttons.json");
@@ -220,13 +220,17 @@ async function createNewCategory(name){
 //Create new sound
 async function createNewSound(){
   var data = await getJSON("buttons.json");
-
+  fileName = await handleFileUpload();
   //Load name and category from request
   const fancyName = document.getElementById("fancyName").value;
-  const newAudioCategory = document.getElementById("newAudioCategory").value;
+  const audioCategory = document.getElementById("newAudioCategory").value;
+  const color = document.getElementById("select-color").value
   //Put config for button into corresponding category
-  data[newAudioCategory].files.push(fileName);
-  data[newAudioCategory].fancy.push(fancyName);
+  data[audioCategory].files.push(fileName);
+  data[audioCategory].fancy.push(fancyName);
+  data[audioCategory].color.push(color);
+  test = data[audioCategory]
+  console.log(test)
   saveJSON(data, "buttons.json");
   buttonSettings();
 }
@@ -235,12 +239,9 @@ async function createNewSound(){
 function handleFileUpload(event){
   const fs = require('fs');
   const reader = new FileReader();
-  const files = event.target.files;
-  
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    fileName = file.name;
-    const filePath = `audio/${fileName}`;
+  const file = document.getElementById("inputFile").files[0]
+  const filePath = `audio/${file.name}`;
+
     reader.onload = function() {
       fs.writeFile(filePath, Buffer.from(reader.result), (err) => {
         if (err) {
@@ -251,8 +252,9 @@ function handleFileUpload(event){
       });
     };
     reader.readAsArrayBuffer(file);
+    return file.name
   }
-}
+
 
 //Mix audio from sound effect with user microphone
 async function mixMicWithAudio(){
@@ -270,14 +272,14 @@ async function mixMicWithAudio(){
 
 //Show alert for reset do default
 function showAlert() {
-  var confirmResult = confirm("Are you sure you want to reset? This CAN NOT be undone!");
+  var confirmResult = confirm(translator.translateForKey('settings.alert-factory-settings',translator._currentLanguage));
   if (confirmResult == true) {
     createJSON("buttons.json");
     createJSON("settings.json");
   }
 }
 
-//Set translation
+//Create HTML menu for language
 async function languageSelect(){
   const languages = await getJSON("languages.json");
   const languageSelect = document.getElementById('languageSelect')
@@ -290,31 +292,110 @@ async function languageSelect(){
     languageSelect.appendChild(option);
   };
 }
+
+//Change site language
+async function changeLanguage(){
+  const data = await getJSON("settings.json")
+  const lang = await getJSON("languages.json");
+  translator.fetch(lang[0].code).then(() => {
+    translator.translatePageTo(data[0].general.languageCode);
+});
+}
+async function exportFile() {
+  const { dialog } = require('@electron/remote');
+  const path = require('path')
+  const fs = require('fs');
+  const file = document.getElementById("export").value;
+  const data = await getJSON(file);
+
+  console.log(data)
+  dialog.showSaveDialog({
+    defaultPath: path.join(__dirname, file),
+    title: 'Save ' + file + ' file',
+    filters: [
+      { name: 'JSON', extensions: ['json'] }
+    ],
+    properties: []
+}).then(file => {
+    console.log(file.canceled);
+    if (!file.canceled) {
+        console.log(file.filePath.toString());
+        // Saving file to selected location
+        fs.writeFile(file.filePath.toString(), JSON.stringify(data), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
+    }
+}).catch(err => {
+    console.log(err)
+});
+};
+
+async function importFile(){
+  const fs = require('fs')
+  const type = document.getElementById("import").value; 
+  const inputFile = document.getElementById("inputFile")
+  const file = inputFile.files[0];
+  console.log(inputFile.files);
+  const reader = new FileReader();
+  reader.readAsText(file);
+  reader.onload = async function() {
+    switch(type){
+      case "buttons.json":
+        await fs.promises.writeFile("cfg/buttons.json", reader.result, "utf8");
+        break;
+      case "settings.json":
+        await fs.promises.writeFile("cfg/settings.json", reader.result, "utf8");
+        break;
+      default:
+        console.log("Unknown file: " + inputFile.files[0].name);
+    }
+  };
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Generating HTML
 async function htmlSettings() {
   mixMicWithAudio();
+  changeLanguage();
   const data = await getJSON("settings.json");
-  let html = '<h1 data-i18n-key="settings">SETTINGS</h1>';
+  let html = '<h1 data-i18n="settings.name">SETTINGS</h1>';
 
     //generate buttons for general settings(language, etc)
-    html += '<h2>General Settings</h2>';
-    html += `Language: ${data[0].general.language}`;
-    html += '<br><br>Select your desired language:';
+    html += '<h2 data-i18n="settings.general">General Settings</h2>';
+    html += translator.translateForKey('general.language',translator._currentLanguage) + data[0].general.languageName + "<br><br>";
+    html += translator.translateForKey('settings.change-language',translator._currentLanguage);
     html += '<select id="languageSelect"></select>';
     
     //generate buttons for audio settings(device output, etc)
-    html += '<h2>Audio Settings</h2>';
-    html += `Current audio output: ${data[0].audio.output}<br>`;
+    html += '<h2 data-i18n="settings.audio">Audio Settings</h2>';
+    html += translator.translateForKey('settings.current-audio-output',translator._currentLanguage) + data[0].audio.output +"<br>";
     html += '<select class="audio-select" id="audioOutputSelect"></select>';
-    html += `<br><br>Current audio output: ${data[0].audio.input}<br>`;
+    html += '<br><br>'
+    html += translator.translateForKey('settings.current-audio-input',translator._currentLanguage) + data[0].audio.input +"<br>";
     html += '<select id="audioInputSelect"></select>';
-    html += `<br><br>Allow microphone passthrough: `;
+    html += '<br><br>'
+    html += translator.translateForKey('settings.allow-mic',translator._currentLanguage);
     html += `<input type="checkbox" id="allowMixing"></input>`;
-    html += `<br><br>Change audio file volume: `;
+    html += '<br><br>'
+    html += translator.translateForKey('settings.change-audio-volume',translator._currentLanguage);
     html += `<input type="range" value="${data[0].audio.fileVolume}" id="audioFileVolume"></input>`;
-    html += '<br><button id="save" class="btn btn-save" onClick="saveSettings()">Save</button> <button class="btn btn-primary" onClick="buttonSettings()">Edit buttons</button> <button id="cancel" class="btn btn-cancel" onClick="htmlButtons()">Cancel</button>'
-    html += `<br><button id="save" class="btn btn-reset" onClick="showAlert()">Reset to factory settings</button>`
+    html += '<br><button id="save" data-i18n="general.save" class="btn btn-save" onClick="saveSettings()">Save</button> <button class="btn btn-primary" data-i18n="settings.buttons" onClick="buttonSettings()">Edit buttons</button> <button id="cancel" data-i18n="general.cancel" class="btn btn-cancel" onClick="htmlButtons()">Cancel</button>'
+    
+    //Import/export settings and factory reset
+    html += '<h1 data-i18n="settings.backup">Export/Import settings</h1>';
+    html += translator.translateForKey('settings.export-select',translator._currentLanguage)
+    html += '<select id="export">';
+    html += '<option value="buttons.json">'+ translator.translateForKey('general.buttons',translator._currentLanguage) + '</options>'
+    html += '<option value="settings.json">' + translator.translateForKey('settings.name',translator._currentLanguage) + '</options></select> '
+    html += '<button data-i18n="settings.export-file" class="btn btn-export" onClick="exportFile()"></button>'
+    html += '<br><br>'
+    html += translator.translateForKey('settings.import-select',translator._currentLanguage)
+    html += '<select id="import">';
+    html += '<option value="buttons.json">'+ translator.translateForKey('general.buttons',translator._currentLanguage) + '</options>'
+    html += '<option value="settings.json">' + translator.translateForKey('settings.name',translator._currentLanguage) + '</options></select>  '
+    html += '<input type="file" id="inputFile" style="display: none" accept="application/JSON"> <label for="inputFile" class="btn btn-selectFile">' + translator.translateForKey('settings.choose-file',translator._currentLanguage) +  '</label> <button class="btn btn-import"onclick="importFile()">' + translator.translateForKey('settings.upload-file',translator._currentLanguage) + '</button>';
+    html += '<br><br>'
+    html += `<br><button data-i18n="settings.reset-factory" class="btn btn-reset" onClick="showAlert()">Reset to factory settings</button>`
   
 
     //Call functions to fill select element
@@ -329,56 +410,69 @@ async function htmlSettings() {
 //Settings for buttons
 async function buttonSettings(){
   mixMicWithAudio();
+  changeLanguage();
   const value = await getJSON("buttons.json");
-  let html = '<h1>Buttons settings</h1>';
-  html += '<h2>Categories <button id="createNewCategory" class="btn btn-newCategory" onClick="htmlNewCategory()">Create New Category</button></h2>';
+  let html = '<h1 data-i18n="settings.buttons">Buttons settings</h1>';
+  html += '<h2> ';
+  html += translator.translateForKey('createNew.categories',translator._currentLanguage)
+  html += ' <button id="createNewCategory" data-i18n="createNew.category" class="btn btn-newCategory" onClick="htmlNewCategory()">Create New Category</button></h2>'
   value.forEach(function(nazev, index){
-    html += `${nazev.category} - <span><button id="${index}" class="btn btn-delete" onClick="removeCategory(this.id)">Remove</button></span><p>`
+    html += `${nazev.category} - <span><button id="${index}" class="btn btn-delete" data-i18n="createNew.delete" onClick="removeCategory(this.id)">Remove</button></span><p>`
     });
-    html += '<h2>Sounds <button id="addNewSound" class="btn btn-newCategory" onClick="htmlNewSound()">Add new sound</button></h2>';
+    html += '<h2> ';
+    html += translator.translateForKey('createNew.sounds',translator._currentLanguage)
+    html += ' <button data-i18n="createNew.sound" id="addNewSound" class="btn btn-newCategory" onClick="htmlNewSound()">Add new sound</button></h2>';
     value.forEach(function(nazev){
       nazev.files.forEach(function(file, index){
-        html += `<span>${nazev.fancy[index]} - <button id="${file}" class="btn btn-delete" onClick="removeSound(this.id)">Remove</button></span><p> `
+        html += `<span>${nazev.fancy[index]} - <button id="${file}" data-i18n="createNew.delete" class="btn btn-delete" onClick="removeSound(this.id)">Remove</button></span><p> `
       });
     });
-  html += '<br><button id="cancel" class="btn btn-cancel" onClick="htmlButtons()">Go back</button>'
+  html += '<center><button data-i18n="general.go-back" class="btn btn-cancel" onClick="htmlButtons()">Go back</button>'
   output.innerHTML = html;
 }
 //Create new HTML for adding audio sounds
-async function htmlNewSound(value){
+async function htmlNewSound(){
   mixMicWithAudio();
-  let html = '<h1>Add new category</h1>';
-  html += 'Enter sound effect name: '
-  html += '<input type="text" id="fancyName"><p>'
-  html += 'Select category: '
+  changeLanguage();
+  let html = '<h1 data-i18n="createNew.sound">Add new sound effect</h1>';
+  html += translator.translateForKey('createNew.sound-name',translator._currentLanguage)
+  html += '<input type="text" id="fancyName">'
+  html += '<br><br>'
+  html += translator.translateForKey('createNew.select-category',translator._currentLanguage)
   html += '<select id="newAudioCategory">';
   const categories = await getJSON("buttons.json");
   categories.forEach(function(category, index) {
     html += `<option value="${index}">${category.category}</option>`;
   });
-  html += '</select><p>';
-  html += 'Upload your audio file: '
-  html += '<input type="file" onchange="handleFileUpload(event)">'
-  html += '<br><button class="btn btn-save" onClick="createNewSound()">Save</button> <button id="cancel" class="btn btn-cancel" onClick="buttonSettings()">Cancel</button>'
+  html += '</select>';
+  html += '<br><br>'
+  html += translator.translateForKey('createNew.select-color',translator._currentLanguage)
+  html += '<input type="color" id="select-color" value="#004FFF" />'
+  html += '<br><br>'
+  html += translator.translateForKey('createNew.upload-file',translator._currentLanguage)
+  html += '<input type="file" id="inputFile" style="display: none" accept=".mp3,audio/*"> <label for="inputFile" class="btn btn-selectFile">' + translator.translateForKey('settings.choose-file',translator._currentLanguage) +  '</label>'
+  html += '<br><button class="btn btn-save" data-i18n="general.save" onClick="createNewSound()">Save</button> <button id="cancel" data-i18n="general.cancel" class="btn btn-cancel" onClick="buttonSettings()">Cancel</button>'
   output.innerHTML = html;
 }
 async function htmlNewCategory(){
   mixMicWithAudio();
-  let html = '<h1>Add new category</h1>';
-  html += 'Enter category name: '
+  changeLanguage();
+  let html = '<h1 data-i18n="createNew.category">Add new category</h1>';
+  html += translator.translateForKey('createNew.category-name',translator._currentLanguage) ;
   html += '<input type="text" id="newCategoryName">'
-  html += '<br><button class="btn btn-save" onClick="createNewCategory()">Save</button> <button id="cancel" class="btn btn-cancel" onClick="buttonSettings()">Cancel</button>'
+  html += '<br><button class="btn btn-save" data-i18n="general.save" onClick="createNewCategory()">Save</button> <button id="cancel" class="btn btn-cancel" data-i18n="general.cancel" onClick="buttonSettings()">Cancel</button>'
   output.innerHTML = html;
 }
 //Generating HTML for buttons
 async function htmlButtons(){
   const data = await getJSON("buttons.json")
   mixMicWithAudio();
-  let html = '<span class="header"><button id="stop" class="btn btn-stop" onClick="stop()">STOP PANIK EMERGENCY</button><button id="settings" class="btn btn-primary" onClick="htmlSettings();">Settings</button></span>';
+  changeLanguage();
+  let html = '<span class="header"><button id="stop" data-i18n="general.stop-panic" class="btn btn-stop" onClick="stop()">STOP PANIK EMERGENCY</button><button data-i18n="settings.name"class="btn btn-primary" onClick="htmlSettings();">Settings</button></span>';
   data.forEach(function(buttons){
     html += `<h2 style="text-align: center;">${buttons.category}</h2><p>`
     buttons.files.forEach(function(file, index){
-      html += `<span><button id="${file}" class="btn btn-primary" onClick="play(this.id)">${buttons.fancy[index]}</button></span> `
+      html += `<span><button id="${file}" class="btn btn-primary" style="background-color: ${buttons.color[index]}" onClick="play(this.id)">${buttons.fancy[index]}</button></span> `
     });
   });
   output.innerHTML = html;
